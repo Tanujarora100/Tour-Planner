56958ab14445ca9cc772318f127451127604b1cc
Tanuj Arora <Tanujarora2703@gmail.com>
2024-01-13 02:52:50 +0530

    Not Interested Functionality Added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,22 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 
 const Card = ({ tour }) => {
     const [readMore, setReadMore] = useState(false);
-   const[text,setText]=useState('read more');
+    const [text, setText] = useState('read more');
+    const [desc, setDesc] = useState(tour.info.substring(0, 200));
+    
 
-    const [desc, setDesc] = useState(tour.info.substring(0,200));
+  
 
     const toggleReadMore = () => {
         setReadMore(!readMore);
         setText(readMore ? 'read more' : 'show less');
-        setDesc(readMore ? tour.info.substring(0,200) : tour.info);
+        setDesc(readMore ? tour.info.substring(0, 200) : tour.info);
     };
 
-    const handleNotInterested = () => {
-        // Implement logic to handle "Not Interested" button click
-        console.log(`Not Interested in ${tour.name}`);
-    };
 
     return (
-        <div className="card">
+        <div className=" border-solid border-black-300">
             <img src={tour.image} alt={tour.name} />
             <h3>{tour.name}</h3>
             <div>
@@ -27,13 +24,14 @@ const Card = ({ tour }) => {
             </div>
             <div className='description'>
                 {desc}
-                <br></br>
-                <br></br>
-                <span className='read-more' onClick={toggleReadMore} >
+                <br />
+                <br />
+                <span className='read-more' onClick={toggleReadMore}>
                     {text}
                 </span>
             </div>
-            <button onClick={handleNotInterested}>Not Interested</button>
+           
+
         </div>
     );
 };
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import Card from './Card'; // Make sure to replace './Card' with the correct path to your Card component
+import React, { useState, useEffect } from 'react';
+import Card from '../components/Card';
 
 const Tours = ({ tours }) => {
-  return (
-    <div className='flex '>
-      {tours.map((tour) => {
-        return <Card key={tour.id} tour={tour} />;
-      })}
-    </div>
-  );
+    const [places, setPlaces] = useState(tours);
+
+    const handleNotInterested = (id) => {
+        setPlaces(prevPlaces => prevPlaces.filter(place => place.id !== id));
+    };
+
+    useEffect(() => {
+        console.log("Places Changed");
+    }, [places]);
+
+    return (
+        <div className="flex">
+            {places.map((tour) => (
+                <div key={tour.id}>
+                    <Card tour={tour} />
+                    <button onClick={() => handleNotInterested(tour.id)}>Not Interested</button>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Tours;
